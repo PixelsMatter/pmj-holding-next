bc1e810ea3b8bb6b77eaa601e73f21ab41a90591
Leon Burman シ <pixelsmatter1@gmail.com>
2023-08-16 14:55:56 +0100

    Add OpenGraph and Twitter meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,32 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'PMJ Building Services',
   description: 'All aspects of building work undertaken.',
+  openGraph: {
+    title: 'PMJ Building Services',
+    description: 'All aspects of building work undertaken.',
+    siteName: 'PMJ Building Services',
+    url: 'https://pmjbuildingservices.co.uk',
+    locale: 'en_GB',
+    images: [
+      {
+        url: 'https://pmjbuildingservices.co.uk/social/thumbnail.png',
+      },
+    ],
+  },
+  twitter: {
+    title: 'PMJ Building Services',
+    description: 'All aspects of building work undertaken.',
+    card: 'summary',
+    creator: '@pmjbuilding',
+    creatorId: '1689363542997151744',
+    site: '@pmjbuilding',
+    siteId: '1689363542997151744',
+    images: [
+      {
+        url: 'https://pmjbuildingservices.co.uk/social/thumbnail.png',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
